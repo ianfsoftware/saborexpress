eaa1e39504a99d3851437c75cc2e7e84dd64ff7d
ianfsoftware <ianfernandes.contato@gmail.com>
2026-05-14 22:43:32 -0400

    Primeiro Commit

---
+++ app.py
@@ -0,0 +1,9 @@
+print('Sabor Express\n')
+
+print("1. Cadastrar Restaurante")
+print("2. Listar Restaurantes")
+print("3. Ativar restaurante")
+print("4. Sair\n")
+
+opcao_escolhida = input("Escolha uma opção: ")
+print ("Você escolheu a opção: " , opcao_escolhida)
