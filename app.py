fc0304d7205386b34b165a313c71ed0bb311dac3
ian Fernandes Sousa <ianfernandes.contato@gmail.com>
2026-05-21 17:28:45 -0400

    funcoes

--- app.py
+++ app.py
@@ -1,11 +1,46 @@
-print('Sabor Express\n')
+import os
 
-print("1. Cadastrar Restaurante")
-print("2. Listar Restaurantes")
-print("3. Ativar restaurante")
-print("4. Sair\n")
+def exibir_nome_do_programa():
+   
+    print("""
 
-opcao_escolhida = input("Escolha uma opção: ")
-print ("Você escolheu a opção: " , opcao_escolhida)
 
-print("\nObrigado por usar o Sabor Express!")
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
+
+""")
+
+def exibir_opcoes():
+    print("1. Cadastrar Restaurante")0
+    print("2. Listar Restaurantes")
+    print("3. Ativar restaurante")
+    print("4. Sair\n")
+
+def finalizar_app():
+    os.system('cls')
+    print("Finalizando o aplicativo\n")
+
+def escolher_opcao():
+    opcao_escolhida = int(input("Escolha uma opção: "))
+    # opcao_escolhida = int(opcao_escolhida)
+
+    if opcao_escolhida == 1:
+        print("Cadastrar Restaurante")
+    elif opcao_escolhida == 2:
+        print("Listar Restaurantes")
+    elif opcao_escolhida == 3:
+        print("Ativar restaurante")
+    else:
+        finalizar_app()
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == "__main__":
+    main()
